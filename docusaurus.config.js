83a4dc54181b82cdac75dd001523fb75b461c827
songhaozhi <hi@songhaozhi.com>
2023-03-03 21:06:49 +0800

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,14 +41,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/weile-dev/docs/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/weile-dev/docs/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
